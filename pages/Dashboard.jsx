c93c6d9a15edc433cd8a59e229cecfacf3cfaf24
William <wlouislouisy@gmail.com>
2022-02-04 10:00:18 +0100

    Homepage improved

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -11,14 +11,20 @@ export default function Dashboard() {
       <div className="flex flex-col items-center my-16 gap-6 md:gap-10 md:flex-row md:flex-wrap md:justify-center">
         <Link passHref href="/AddMovies">
           <div className="hover:scale-110 duration-300 cursor-pointer overflow-hidden relative w-[260px] h-[180px] border-2 border-knicksBlue rounded-xl bg-movies bg-cover flex justify-center items-center shadow-2xl">
-            <h4 className="text-knicksOrange text-center text-2xl font-black bg-knicksBlue w-full flex justify-center items-center p-2 absolute bottom-0">
+            <h4
+              className="text-knicksOrange text-center text-2xl font-black bg-knicksBlue w-[260px]
+[260px] flex justify-center items-center p-2 absolute bottom-0"
+            >
               MANAGE MOVIES
             </h4>
           </div>
         </Link>
         <Link passHref href="/AddSeries">
           <div className="hover:scale-110 duration-300 cursor-pointer overflow-hidden relative w-[260px] h-[180px] border-2 border-knicksBlue rounded-xl bg-series bg-cover flex justify-center items-center shadow-2xl">
-            <h4 className="text-knicksOrange text-center text-2xl font-black bg-knicksBlue w-full flex justify-center items-center p-2 absolute bottom-0">
+            <h4
+              className="text-knicksOrange text-center text-2xl font-black bg-knicksBlue w-[260px]
+[260px] flex justify-center items-center p-2 absolute bottom-0"
+            >
               MANAGE SERIES
             </h4>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,19 @@ export default function Home() {
         <h1 className="text-center font-black text-7xl md:text-8xl text-knicksBlue">
           CHECK <span className="text-knicksOrange -rotate-6"> THIS</span> ONE
         </h1>
-        <div className="bg-knicksBlue w-full px-8 py-16 bubble text-center">
-          <p className="text-2xl md:text-4xl">
+        <div className="bg-knicksBlue w-full px-8 py-16 text-center md:mt-16">
+          <p className="text-3xl md:text-4xl">
             I am glad to show you my first
             <span className="text-knicksOrange"> personal</span> project.
           </p>{" "}
           <br />
-          <p className="text-2xl md:text-4xl">
+          <p className="text-4xl md:text-5xl">
             Create a website that would allow me to share my
             <span className="text-knicksOrange"> recommendations </span>
-            about movies, series, Youtube channels that I am use to spread
-            around me...
+            about movies, series, that I am use to spread around me...
           </p>{" "}
           <br />
-          <p className="text-3xl md:text-6xl">
+          <p className="text-5xl md:text-6xl">
             And maybe more in the
             <span className="text-knicksOrange"> future</span>.
           </p>
