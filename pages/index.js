9a836ab5432dee8e2e68e37c2eed9a777963aa8e
William <wlouislouisy@gmail.com>
2022-03-12 18:14:22 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout pageTitle="HomePage">
-      <div className="px-4 flex flex-col items-center my-24 gap-6 md:gap-10 md:flex-row md:flex-wrap md:justify-center">
+      <div className="px-4 flex flex-col items-center my-24 gap-6 md:gap-10 md:justify-center">
         <h1 className="text-center font-black text-7xl md:text-8xl text-knicksBlue">
           CHECK <span className="text-knicksOrange -rotate-6"> THIS</span> ONE
         </h1>
-        <div className="bg-knicksBlue w-full px-8 py-16 text-center md:mt-16">
+        <div className="bg-knicksBlue w-3/5 px-8 py-16 text-center md:mt-16 rounded-t-[60px] rounded-b-[210px]">
           <p className="text-3xl md:text-4xl">
             I am glad to show you my first
             <span className="text-knicksOrange"> personal</span> project.
